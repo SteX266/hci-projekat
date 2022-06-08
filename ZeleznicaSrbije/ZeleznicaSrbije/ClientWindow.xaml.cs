f994e3ca1914ff244fd6d09912a4996b233804be
SteX266 <stefan.milosevic.e14@gmail.com>
2022-06-08 17:12:11 +0200

    Dodat prozor za klijenta i hamburger meni

--- ZeleznicaSrbije/ZeleznicaSrbije/ClientWindow.xaml.cs
+++ ZeleznicaSrbije/ZeleznicaSrbije/ClientWindow.xaml.cs
@@ -10,35 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ZeleznicaSrbije
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ClientWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-
-
+    public partial class ClientWindow : Window
     {
-        public Frame Frame { get; set; }
-        public MainWindow()
+        public ClientWindow()
         {
             InitializeComponent();
-
-
-        }
-
-        private void LocalLoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            ClientWindow clientWindow = new ClientWindow();
-            clientWindow.Show(); 
-        }
-
-        private void CheckBoxChanged(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
